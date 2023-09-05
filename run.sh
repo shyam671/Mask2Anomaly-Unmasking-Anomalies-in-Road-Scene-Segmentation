c9ca4de8df35649c148e8041219ce9b4ff4f1f3e
Shyam Nandan Rai <shyamnandanraii@gmail.com>
2023-09-05 18:17:16 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 CUDA_VISIBLE_DEVICES=1 python train_net.py --num-gpus 1  --config-file configs/cityscapes/semantic-segmentation/anomaly_train.yaml
 #Anomaly Fine-Tune
 CUDA_VISIBLE_DEVICES=1 python train_net.py --num-gpus 1  --config-file configs/cityscapes/semantic-segmentation/anomaly_ft.yaml
+
+
+## Anomaly Inference
 #RoadAnomaly21
 CUDA_VISIBLE_DEVICES=1 python anomaly_utils/anomaly_inference.py --input '/home/shyam/Mask2Former/unk-eval/RoadAnomaly21/images/*.png' --config-file '/home/shyam/Mask2Anomaly/configs/cityscapes/semantic-segmentation/anomaly_inference.yaml' 
 #RoadObsticle21
