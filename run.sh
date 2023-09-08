960339c9f30516700fc38c0c37043de4aecf1190
Shyam Nandan Rai <shyamnandanraii@gmail.com>
2023-09-08 18:22:42 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,3 +15,10 @@ CUDA_VISIBLE_DEVICES=1 python anomaly_utils/anomaly_inference.py --input '/home/
 CUDA_VISIBLE_DEVICES=1 python anomaly_utils/anomaly_inference.py --input '/home/shyam/Mask2Former/unk-eval/fs_static/images/*.jpg' --config-file '/home/shyam/Mask2Anomaly/configs/cityscapes/semantic-segmentation/anomaly_inference.yaml' 
 #RoadAnomaly
 CUDA_VISIBLE_DEVICES=1 python anomaly_utils/anomaly_inference.py --input '/home/shyam/Mask2Former/unk-eval/RoadAnomaly/images/*.jpg' --config-file '/home/shyam/Mask2Anomaly/configs/cityscapes/semantic-segmentation/anomaly_inference.yaml' 
+
+#StreetHazard Training
+CUDA_VISIBLE_DEVICES=1 python train_net.py --num-gpus 1 --config-file '/home/shyam/Mask2Anomaly/configs/streethazard/streethazard_training.yaml'
+#StreetHazard Fine-Tuning
+CUDA_VISIBLE_DEVICES=1 python train_net.py --num-gpus 1 --config-file '/home/shyam/Mask2Anomaly/configs/streethazard/streethazard_ft.yaml' 
+#StreetHazard Inference
+CUDA_VISIBLE_DEVICES=1 python anomaly_utils/anomaly_inference_streethazard.py --input '/home/shyam/Mask2Anomaly/datasets/streethazard/test/images/*.png' --config-file '/home/shyam/Mask2Anomaly/configs/streethazard/streethazard_inference.yaml' --weights '/home/shyam/Mask2Former/output/streethazard-swin-base-coco-supervised/best_model.pth'
